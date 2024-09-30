63cc52de9b588434813301abc0b0fb7e056ce493
Timothy Efraim Hotasi <timothyefraim35@gmail.com>
2024-09-30 14:51:51 +0700

    exercise 3.4, 3.5, and 3.6 done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const data = [
+let data = [
   { 
     "id": "1",
     "name": "Arto Hellas", 
@@ -24,6 +24,14 @@ const data = [
 const express = require('express')
 const app = express()
 
+// Important to convert JSON into JavaScript object 
+// to then, be attached into request.body
+app.use(express.json())
+
+const getId = () => {
+  return Math.floor(Math.random() * 1000000)
+}
+
 app.get('/api/persons', (request, response) => {
   response.json(data)
 })
@@ -31,7 +39,12 @@ app.get('/api/persons', (request, response) => {
 app.get('/api/person/:id', (request, response) => {
   const id = request.params.id
   const personData = data.find(p => p.id === id)
-  response.json(personData)
+  
+  if (personData) {
+    response.json(personData)
+  } else {
+    response.status(404).end()
+  }
 })
 
 app.get('/info', (request, response) => {
@@ -42,6 +55,50 @@ app.get('/info', (request, response) => {
     `<p>Phonebook has info for ${data.length} people</p>
     <p>${today.toString()}<p>`
   )
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body) {
+    return response.status(400).json({
+      error: 'entry data missing'
+    })
+  } 
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name mising'
+    })
+  } 
+  
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number mising'
+    })
+  }
+  
+  if (data.find(p => p.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const newPerson = {
+    "id": getId(),
+    "name": body.name,
+    "number": body.number
+  }
+
+  data = data.concat(newPerson)
+  response.json(data)
+
+})
+
+app.delete('/api/person/:id', (request, response) => {
+  const id = request.params.id
+  data = data.filter(p => p.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
